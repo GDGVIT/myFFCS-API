2ee71e09475a2cdce3716950b23ecee5dcf5064d
Mounish Sai <pvsmounish@gmail.com>
2019-03-27 23:20:49 +0530

    Added User Model

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose    = require("mongoose");
+const mongoose = require("mongoose");
 const app = express();
 
 mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true});
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 mongoose.connection.once('open', () => {
     console.log('Connected to Database');
-})
+});
+
+const indexRoutes = require('./routes/index');
+
+app.use('/', indexRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log('myFFCS Server API Started');
+});
---
+++ models/user/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: String,
+    timetables: [[Number]],
+    createdAt: { type: Date, default: Date.now },
+    lastUpdatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
