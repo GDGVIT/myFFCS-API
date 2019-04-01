500c6f8bdac1933636659ad3afa0733a4744ed7b
Mounish Sai <pvsmounish@gmail.com>
2019-04-01 17:07:32 +0530

    Added classRoom/searchbycoursename

--- routes/classRoom.js
+++ routes/classRoom.js
@@ -19,6 +19,23 @@ router.get('/searchbyfaculty/:keyword', (req, res) => {
 
 });
 
+router.get('/searchbycoursename/:keyword', (req, res) => {
+
+    const keyword = req.params.keyword.toLowerCase();
+
+    try {
+        const filteredData = ffcsData.filter((classRoom) => {
+            return classRoom.TITLE.toLowerCase().indexOf(keyword) >= 0 ? true: false;
+        });
+
+        return res.status(200).json(filteredData);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send('Error');
+    }
+
+});
+
 router.get('/:classRoomId', (req, res) => {
 
     try {
